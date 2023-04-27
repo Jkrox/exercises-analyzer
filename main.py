60d7fb07e5685bcd421fb41d2ef4c56595fca190
Jkrox <jkrox@protonmail.com>
2023-04-26 23:16:41 -0500

    Translating everything from Spanish to English

--- main.py
+++ main.py
@@ -1,61 +1,61 @@
 """
-Proyecto: 
-Dada una meta de entrenamiento específica (ya sea aumentar músculo o perder grasa), el peso y altura de una persona,
-y un presupuesto para la alimentación de un mes, ¿cuál sería la mejor combinación de ejercicios y alimentos para
-lograr esa meta y ajustarse al presupuesto?
+Project: 
+Given a specific training goal (whether to gain muscle or lose fat), a person's weight and height, 
+and a budget for one month's food, what would be the best combination of exercises and foods to 
+achieve that goal and stay within the budget?
 """
 
-import csv, os
+import csv
+import os
 from prettytable import PrettyTable
 from dataclasses import dataclass
 
-def user_input() -> list:
+
+def get_user_input() -> list:
     preview: str = """
     ----------------------------------------------------
 |                                                  |
-|      Bienvenido al planificador de entrenamiento  |
+|          Welcome to the training planner          |
 |                                                  |
 ----------------------------------------------------
 |                                                  |
-|        ¿Cuál es tu objetivo de entrenamiento?     |
+|        What is your training goal?                |
 |                                                  |
-|             [P] Perder peso                        |
-|             [S] Subir masa muscular               |
+|             [L] Lose weight                        |
+|             [G] Gain muscle                       |
 |                                                  |
-----------------------------------------------------
-    """
+---------------------------------------------------- """
     while True:
         print(preview)
         
-        option = input("Seleccionar la opción que aparece en pantalla: ").lower()
-        weight = float(input("Cuál es su peso en kilogramos: "))
-        height = int(input("Cuál es su altura en centimetros: "))
+        option = input("Select an option from above: ").lower()
+        weight = float(input("What is your weight in kilograms? "))
+        height = int(input("What is your height in centimeters? "))
         
-        if option != "p" and option != 's':
-            raise ValueError('Opción no válida')     
-        elif option == 'p' or option == 's':
+        if option not in ["l", "g"]:
+            raise ValueError('Invalid option selected.')     
+        else:
             os.system("cls")
-            if option == 'p':
-                print("Elegiste: Perder peso. Bien, comencemos!")
-                
+            if option == 'l':
+                print("You chose: Lose weight. Let's get started!")
             else:
-                print("Elegiste: Subir masa muscular. Bien, comencemos!")
-                
+                print("You chose: Gain muscle. Let's get started!")
             return [option, weight, height]    
             
+
 @dataclass
-class ExercisesHeaders:
+class ExerciseHeaders:
     name: str
-    tipo: str
-    nivel: str
-    calorias_quemadas: int
+    type_: str
+    level: str
+    calories_burnt: int
     
 @dataclass
-class MealsHeaders:
-    alimento: str
-    calorias: int
-    carbohidratos: int
-    grasas: int
+class MealHeaders:
+    food: str
+    calories: int
+    carbohydrates: int
+    fats: int
     
 class Calculator:
     def __init__(self, exercises_file: str, meals_file: str) -> None:
@@ -72,33 +72,32 @@ class Calculator:
         with open(meals_file, 'rt', encoding='utf-8') as meals:
             meals_data_raw = csv.reader(meals)
             self.meals = list(meals_data_raw) 
-            print(self.meals) 
             
     def show_data(self) -> str:
         """We print out the exercises and meals in a pretty way and colorful.
 
         Returns:
-            str: Table representing our data more organised for the view.
+            str: Table representing our data more organized for the view.
         """
-        table, table2 = PrettyTable(), PrettyTable()
-        table.field_names, table2.field_names = self.exercises[0], self.meals[0]
+        table_exercises, table_meals = PrettyTable(), PrettyTable()
+        table_exercises.field_names, table_meals.field_names = self.exercises[0], self.meals[0]
         rows = list(zip(self.exercises[1:], self.meals[1:]))
-        for row, row2 in rows:
-            table.add_row(row)
-            table2.add_row(row2)
-        return str(table), str(table2) # table = Exercises, table2 = Meals
+        for row_exercises, row_meals in rows:
+            table_exercises.add_row(row_exercises)
+            table_meals.add_row(row_meals)
+        return str(table_meals), str(table_exercises) # table_meals = Meals, table_exercises = Exercises
              
     
     def is_food(self):
-        meal1 = MealsHeaders(alimento=self.meals[0])
+        meal1 = MealHeaders(food=self.meals[0])
     
         
 
 def main():
-    perder_grasa = Calculator(exercises_file="exercises.csv", meals_file='meals.csv')
-    user_input()
-    print(perder_grasa.show_data()[1], '\n')
-    print(perder_grasa.show_data()[0])
+    lose_weight = Calculator(exercises_file="exercises.csv", meals_file='meals.csv')
+    get_user_input()
+    print(lose_weight.show_data()[0], '\n')
+    print(lose_weight.show_data()[1])
     
 if __name__ == "__main__":
     main()
